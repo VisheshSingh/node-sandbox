59da29a654acd66f2c8a2e14dde52bcfa6932c8d
Vishesh <visheshsingh16@gmail.com>
2018-11-27 16:07:50 -0500

    Module Wrapper function :pencil:

--- logger.js
+++ logger.js
@@ -1,3 +1,6 @@
+// (function(exports, require, module, __filename,__dirname){}) --> MODULE WRAPPER FUNCTION
+console.log(__filename);
+console.log(__dirname);
 var url = "http://mylogger.io/logs";
 
 function log(mesg) {
