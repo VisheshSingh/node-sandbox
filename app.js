5bd1d6574dc4d5a42eeac1fb53e1f50172e58cfe
Vishesh <visheshsingh16@gmail.com>
2018-11-27 20:46:00 -0500

    Route params:postal_horn:

--- app.js
+++ app.js
@@ -9,4 +9,9 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/courses/:id/:month/:year", (req, res) => {
+  //   res.send(req.params.id);
+  res.send(req.params);
+});
+
 app.listen(3001, () => console.log("listening to port 3001..."));
