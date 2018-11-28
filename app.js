c4a96a041825512ac01c92ba3b4d6e4c5b5e4949
Vishesh <visheshsingh16@gmail.com>
2018-11-27 20:53:01 -0500

    Handdling HTTP GET Requests :golf:

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "Maths" },
+  { id: 2, name: "Science" },
+  { id: 3, name: "Computers" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.get("/api/courses/:id/:month/:year", (req, res) => {
-  //   res.send(req.params.id);
-  res.send(req.params);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send("The requested course was not found in our database...");
+  else res.send(course);
 });
 
 app.listen(3001, () => console.log("listening to port 3001..."));
