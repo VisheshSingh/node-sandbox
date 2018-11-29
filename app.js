b1129af68c9f1f9215586fad33b664f739775208
Vishesh <visheshsingh16@gmail.com>
2018-11-29 12:02:44 -0500

    Third-Party middleware :art:

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const helmet = require("helmet");
+const morgan = require("morgan");
 const Joi = require("joi");
 const logger = require("./logger");
 const express = require("express");
@@ -7,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// THIRD-PARTY MIDDLEWARE
+app.use(helmet());
+app.use(morgan("tiny"));
 
 // CREATE CUSTOM MIDDLEWARE FUNCTION
 app.use(logger);
